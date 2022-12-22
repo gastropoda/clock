c7f90394c537951b27b9eaf0cdbe1ab795fd9743
Artem Baguinski <a.baguinski@performation.com>
2022-12-22 12:40:18 +0000

    Clean up the script

--- js/clock.js
+++ js/clock.js
@@ -1,10 +1,11 @@
 function updateClock() {
+  let doc = window.document
   let dt = luxon.DateTime.now().setLocale("ru-RU")
-  let time = dt.toFormat("HH:mm");
-  document.getElementById("time").innerText = time;
-  document.getElementById("title").innerText = time;
+  let time = dt.toFormat("HH:mm")
+  doc.getElementById("time").innerText = time
+  doc.getElementById("title").innerText = time
   let date = dt.toFormat("EEEE - d MMMM - неделя WW")
-  document.getElementById("date").innerText = date;
+  doc.getElementById("date").innerText = date
 }
 
 setInterval(updateClock, 1000)
