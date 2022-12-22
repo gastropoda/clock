7f14f5d1b57cc4d4324251ded3e9ca688e68c906
Artem Baguinski <a.baguinski@performation.com>
2022-12-22 13:16:16 +0000

    Allow to switch clock languages

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Clock with a youtube background. Press F to toggle fullscreen. 
+- clock with a youtube background
+- press **F** to toggle fullscreen
+- press **L** to switch to the next language
 
 ![Clock screenshot](/clock-screenshot.png?raw=true "Clock screenshot")
--- js/clock.js
+++ js/clock.js
@@ -1,10 +1,31 @@
+let locales = [{
+  name: "ru-RU",
+  week: "неделя",
+},{
+  name: "nl-NL",
+  week: "week",
+},{
+  name: "en-US",
+  week: "Week",
+}]
+let currentLocaleIndex = 0
+
+function currentLocale() {
+  return locales[currentLocaleIndex];
+}
+
+function nextLocale() {
+  currentLocaleIndex = (currentLocaleIndex+1) % locales.length;
+}
+
 function updateClock() {
+  let locale = currentLocale()
   let doc = window.document
-  let dt = luxon.DateTime.now().setLocale("ru-RU")
+  let dt = luxon.DateTime.now().setLocale(locale.name)
   let time = dt.toFormat("HH:mm")
   doc.getElementById("time").innerText = time
   doc.getElementById("title").innerText = time
-  let date = dt.toFormat("EEEE - d MMMM - неделя WW")
+  let date = dt.toFormat(`EEEE - d MMMM - '${locale.week}' WW`)
   doc.getElementById("date").innerText = date
 }
 
@@ -23,5 +44,10 @@ window.addEventListener("keydown", (event) => {
     let doc = window.document
     let body = doc.getElementsByTagName("body")[0]
     toggleFullscreen(body)
+  }
+
+  if (event.code == "KeyL") {
+    nextLocale()
+    updateClock()
   }
 })
